57a49f101e06d457520fdbb510b4abff112d9f40
Thanh Le <thanh@technocreatives.com>
2022-06-17 13:56:55 +0200

    Fix build by build.sh

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo $1
+echo $2
+
+# build native headers from java source
+ant javah
+
+# find and replace package & headers name
+find ./build/headers/ -type f -exec sed -i -e "s/com_sun_jna/shadow_jna/g" {} \;  # rename within files
+rm ./build/headers/*-e
+mv ./build/headers/com_sun_jna_Native.h ./build/headers/shadow_jna_Native.h
+mv ./build/headers/com_sun_jna_Function.h ./build/headers/shadow_jna_Function.h
+mv ./build/headers/com_sun_jna_win32_DLLCallback.h ./build/headers/shadow_jna_win32_DLLCallback.h
+
+# build native libraries
+ant native
+
+# build jar files
+
+ant jar
